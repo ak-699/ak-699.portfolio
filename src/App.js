f10baf6d0cbf922e555a6f4ff01a20e0e84c9227
ak-699 <kumar699.abhishek@gmail.com>
2024-04-27 05:18:07 +0530

    make a skeletal of website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './Pages/HomePage';
+import { useEffect } from 'react';
 
 function App() {
+
   return (
-    <HomePage />
+    <>
+      <HomePage />
+    </>
   );
 }
 
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -14,23 +14,70 @@ const HomePage = () => {
 
                 <div className="nav-bar">
                     <div className="logo">
-                        {"</> "}AK
+                        <img src="./ak-high-resolution-logo-transparent.png" alt="" />
                     </div>
                     <div className="nav-items">
                         <div className="item"><a href="">Home</a></div>
                         <div className="item"><a href="">About</a></div>
                         <div className="item"><a href="">Projects</a></div>
                         <div className="item"><a href="">Notes</a></div>
-                        <div className="item"><a href="">Contact Me</a></div>
+                        <div className="item contact-me"><a href="">Contact Me</a></div>
                     </div>
                 </div>
 
                 <div className="introduction">
                     <div className="my-roles">
-                        <h2>Fullstack Web Developer</h2>
-                        <h2>AWS Solutions Architect</h2>
+                        <div>
+                            <h2><span>AWS Cloud</span></h2>
+                            <h2>&</h2>
+                            <h2><span>Fullstack Web</span></h2>
+                        </div>
+                        <h2><span>Developer</span></h2>
                     </div>
-                    <div className="description"></div>
+                    <div className="description">
+                        <div>
+                            <p>I am Abhishek</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias eum est quaerat. Ab quibusdam facilis ex alias laboriosam molestiae nihil, blanditiis voluptatem hic incidunt, porro doloribus, qui temporibus provident optio!</p>
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div className="empty-area">
+
+                </div>
+                <div className="achivements">
+                    <h2>Achievents and Certs</h2>
+                    <div className="slideshow">
+                        <div className="cert"></div>
+                        <div className="cert"></div>
+                        <div className="cert"></div>
+                        <div className="cert"></div>
+
+                    </div>
+                </div>
+                <div className="empty-area">
+
+                </div>
+                <div className="personal-projects">
+                    <h2>Personal Projects</h2>
+                    <div className="project-list">
+                        <div className="project">Visual Insight Generator</div>
+                        <div className="project">Fullstack MERN Blog App</div>
+                        <div className="project">Todo-List</div>
+
+                    </div>
+                </div>
+                <div className="empty-area">
+
+                </div>
+                <div className="contact-section">
+                    <p>lets build</p>
+                    <h2><span>AMAZING THINGS</span></h2>
+                    <p>together</p>
+                    <a href="">Contact Me</a>
                 </div>
             </div>
         </div>
